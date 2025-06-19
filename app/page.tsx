a830f3791d59a7b1a0c97e6568c7efedf80713e9
abhishekrathour27 <abhishekrathour96081@gmail.com>
2025-06-19 20:41:48 +0530

    learning routing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="p-5">
-      <h1 className="">Hello</h1>
+      <h1 className="">Hello Developers</h1>
     </div>
   );
 }
---
+++ app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      new user page
+    </div>
+  )
+}
+
+export default page
